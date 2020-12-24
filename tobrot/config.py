06312c212b0f6a76a2d6f317ae1085aee2b89239
zero-debugs <59023237+zero-debugs@users.noreply.github.com>
2020-12-24 20:13:38 +0530

    Create config.py
    Updated

---
+++ tobrot/config.py
@@ -0,0 +1,25 @@
+from tobrot.sample_config import Config
+ 
+class Config(Config):
+  TG_BOT_TOKEN = "1473537270:AAFI0kJCyqcMR7X3cJu51ZSgvjm67jIZLo8 "
+  APP_ID = 2048409 
+  API_HASH = "163QR8Ok92D4UXR2zzII6WXUfB0a2TH_h"
+  AUTH_CHANNEL = [-1001235672935, -1001450960310, -1001260343472,  911646244 ]
+  INDEX_LINK = "https://up.bravelybot.workers.dev/3:/Uploads"
+  YTDL_COMMAND = "ytdl@bravelybot"
+  LEECH_COMMAND = "leech@bravelybot"
+  CLEAR_THUMBNAIL = "clearthumbnail@bravelybot"
+  SAVE_THUMBNAIL = "savethumbnail@bravelybot"
+  OWNER_ID = 715779594
+  RCLONE_CONFIG = """type = drive
+scope = drive
+root_folder_id = 1rWomzK1yVjkqIbC9p9EBBKJgcf3imGr4
+token = {"access_token":"ya29A0AfH6SMCaLwuWkNALk5I4IWBZP6y5Y4lZRkgepXoaf-LvSu2jmNDyLzezwJk-HlgcP1OSiRKqAa9qKsCBMv9O2IMd1ebAwhYdVoNP7jIXMRBDKzD10qfrzq7oGda_Z-vP3Qvl6PQcOYC3x5ecEkU3vMIyqEC2xgANsEnowpSENeU","token_type":"Bearer","refresh_token":"1//0gHp6knwySTm9CgYIARAAGBASNwF-L9IrRPO94mU1d9kBkUu1BbYrkA_EMhGXcCiHXGULiAZDg5BmaSxhrtALRfBmVpiotPEtK6Q","expiry":"2020-11-11T13:38:54.4701885+06:00"}
+team_drive = 1rWomzK1yVjkqIbC9p9EBBKJgcf3imGr4"""
+  DESTINATION_FOLDER = ""
+  GLEECH_COMMAND = "gleech@bravelybot"
+  TELEGRAM_LEECH_COMMAND_G = "tleech@bravelybot"
+  PYTDL_COMMAND_G = "pytdl@bravelybot"
+  MAX_TG_SPLIT_FILE_SIZE = 2097152000
+  CANCEL_COMMAND_G = "cancel@bravelybot"
+  STATUS_COMMAND = "status@bravelybot"
