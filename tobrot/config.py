8bf3c816399800da29ad099f11ab7be822fa1a51
gufimov <72704694+gufimov@users.noreply.github.com>
2021-01-07 19:59:58 +0530

    Update config.py

--- tobrot/config.py
+++ tobrot/config.py
@@ -1,28 +1,26 @@
 from tobrot.sample_config import Config
  
 class Config(Config):
-  TG_BOT_TOKEN = "1473537270:AAFI0kJCyqcMR7X3cJu51ZSgvjm67jIZLo8"
+  TG_BOT_TOKEN = "1553212518:AAFMEEdATxOXyp6aUOuajbnIvrxLgfblzSc"
   APP_ID = 2048409 
   API_HASH = "649e419e5fa739da02b649780d492c1b"
-  AUTH_CHANNEL = [-1001235672935, -1001450960310, -1001260343472, 911646244]
-  INDEX_LINK = "https://up.bravelybot.workers.dev/0:"
-  YTDL_COMMAND = "ytdl@bravesawrhtunnamed"
-  LEECH_COMMAND = "leech@bravelybot"
-  CLEAR_THUMBNAIL = "clearthumbnail@bravelybot"
-  SAVE_THUMBNAIL = "savethumbnail@bravelybot"
+  AUTH_CHANNEL = [-1001437129982]
+  INDEX_LINK = "https://up.iunix.workers.dev"
+  YTDL_COMMAND = "ytdl@iunixbot"
+  LEECH_COMMAND = "leech@iunixbot"
+  CLEAR_THUMBNAIL = "clearthumbnail@iunixbot"
+  SAVE_THUMBNAIL = "savethumbnail@iunixbot"
   OWNER_ID = 1464067320
   RCLONE_CONFIG = """type = drive
 scope = drive
 token = {"access_token":"ya29.a0AfH6SMBtf7kt7rfb2zXGKD8yGgrP2387_sX5Q4I0czgqthfdSldLi4Xh6zogVwcPzch_KUGwK_Ur8FyfzcJegdWM7aQGu3ujp3Z0R4ddVpTl3XqWcGWA4S5VDqcWrcwid-4rEmE658KNMfgMM7WfxMg_BUMmxRuDHh3S8tosCF0","token_type":"Bearer","refresh_token":"1//0gxLXtEcUe5V4CgYIARAAGBASNwF-L9IrlgIaC9FAL6bLZ34Id_hccgZO9WfVm-wIgnKOQHBKfjgRYZX9_4Bb-x8ZHELqo66lmQs","expiry":"2020-12-24T15:50:19.450632145Z"}
 team_drive = 0ADgye8jT_91FUk9PVA"""
   DESTINATION_FOLDER = ""
-  GLEECH_COMMAND = "gleech@bravelybot"
-  TELEGRAM_LEECH_COMMAND_G = "tleech@bravelybot"
-  PYTDL_COMMAND_G = "pytdl@bravesawrhtunnamed"
+  GLEECH_COMMAND = "gleech@iunixbot"
+  TELEGRAM_LEECH_COMMAND_G = "tleech@iunixbot"
+  PYTDL_COMMAND_G = "pytdl@iunixbot"
   MAX_TG_SPLIT_FILE_SIZE = 2097152000
-  CANCEL_COMMAND_G = "cancel@bravelybot"
-  STATUS_COMMAND = "status@bravelybot"
-  MAX_FILE_SIZE = 50000000
-  FREE_USER_MAX_FILE_SIZE = 50000000
-  GET_SIZE_G = "getsize@bravesaw"
-  RENEWME_COMMAND = "renewme@bravesaw" 
+  CANCEL_COMMAND_G = "cancel@iunixbot"
+  STATUS_COMMAND = "status@iunixbot"
+  GET_SIZE_G = "getsize@iunixbot"
+  RENEWME_COMMAND = "renewme@iunixbot" 
