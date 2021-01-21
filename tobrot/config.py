39fcd8fcbe8c38e17ed8e655bef1698cbb1d5af3
qujdev <75565387+qujdev@users.noreply.github.com>
2021-01-21 21:13:22 +0530

    Update config.py

--- tobrot/config.py
+++ tobrot/config.py
@@ -1,26 +1,26 @@
 from tobrot.sample_config import Config
  
 class Config(Config):
-  TG_BOT_TOKEN = "1553212518:AAFMEEdATxOXyp6aUOuajbnIvrxLgfblzSc"
+  TG_BOT_TOKEN = "1569404369:AAHPPjCMc7lccnmv4m5o1VQ-s1PuKhMyqaw"
   APP_ID = 2048409 
   API_HASH = "649e419e5fa739da02b649780d492c1b"
-  AUTH_CHANNEL = [-1001437129982]
-  INDEX_LINK = "https://up.iunix.workers.dev"
-  YTDL_COMMAND = "ytdl@iunixbot"
-  LEECH_COMMAND = "leech@iunixbot"
-  CLEAR_THUMBNAIL = "clearthumbnail@iunixbot"
-  SAVE_THUMBNAIL = "savethumbnail@iunixbot"
+  AUTH_CHANNEL = [-1001287210872]
+  INDEX_LINK = ""
+  YTDL_COMMAND = "ytdl@qunixbot"
+  LEECH_COMMAND = "leech@nottouse"
+  CLEAR_THUMBNAIL = "clearthumbnail@qunixbot"
+  SAVE_THUMBNAIL = "savethumbnail@qunixbot"
   OWNER_ID = 1464067320
   RCLONE_CONFIG = """type = drive
 scope = drive
 token = {"access_token":"ya29.a0AfH6SMCdvunzo0DxBYAGy8bojOu4WRkmVIoJJ6Ccdry1JBn6a_H2LA9Mtc4Kq0pUZhuaAeqS_MLG-rsCVZin0b1dy3270DZw6lXPNdYO2Nq7MF0g4xv9POSVdGv5sVYG3Kd2Hco833CmkY4WWtXMjLfSWr7sS-03pH5laE4B1CQ","token_type":"Bearer","refresh_token":"1//0gvr-t3JX5MMCCgYIARAAGBASNwF-L9IrH7c02VPO7-j0ILN7CvBOp7nJ5KfNnTp6UnhIMb2dM486sIoXkHtO00Tm_4ee53KMPtA","expiry":"2021-01-21T14:02:14.505536378Z"}
 team_drive = 0AMExHmp8NzCUUk9PVA"""
   DESTINATION_FOLDER = "movie"
-  GLEECH_COMMAND = "gleech@iunixbot"
-  TELEGRAM_LEECH_COMMAND_G = "tleech@iunixbot"
-  PYTDL_COMMAND_G = "pytdl@iunixbot"
+  GLEECH_COMMAND = "gleech@nottouse"
+  TELEGRAM_LEECH_COMMAND_G = "tleech@qunixbot"
+  PYTDL_COMMAND_G = "pytdl@qunixbot"
   MAX_TG_SPLIT_FILE_SIZE = 2097152000
-  CANCEL_COMMAND_G = "cancel@iunixbot"
-  STATUS_COMMAND = "status@iunixbot"
-  GET_SIZE_G = "getsize@iunixbot"
-  RENEWME_COMMAND = "renewme@iunixbot" 
+  CANCEL_COMMAND_G = "cancel@qunixbot"
+  STATUS_COMMAND = "status"
+  GET_SIZE_G = "getsize@qunixbot"
+  RENEWME_COMMAND = "renewme@qunixbot" 
